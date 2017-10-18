6ecc0fb197479b19f0319fb72dbfec909db705c6
eoinahern <eoin.ahern@mycit.ie>
2017-10-18 11:38:45 +0100

    added component

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivityComponent.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivityComponent.java
@@ -1,4 +1,20 @@
 package com.example.android.architecture.blueprints.todoapp.tasks;
 
-public interface TasksActivityComponent {
+import com.example.android.architecture.blueprints.todoapp.DI.components.BaseActivityComponent;
+import com.example.android.architecture.blueprints.todoapp.DI.modules.BaseActivityModule;
+
+import dagger.Module;
+import dagger.Subcomponent;
+
+
+@Subcomponent(modules = TasksActivityComponent.TasksActivityModule.class)
+public interface TasksActivityComponent extends BaseActivityComponent<TasksActivity>{
+
+	@Module
+	class TasksActivityModule extends BaseActivityModule<TasksActivity>{
+
+		public TasksActivityModule(TasksActivity activity) {
+			super(activity);
+		}
+	}
 }
